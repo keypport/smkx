6db46420fb5e47ea33980b9efba1ad79c3050269
keypport <keypport@gmail.com>
2023-12-31 07:37:07 +0900

    feat: update predeploy, assetPrefix

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
   },
   assetPrefix:
     process.env.NODE_ENV === 'production'
-      ? 'https://keypport.github.io/smkx/'
+      ? 'https://keypport.github.io/smkx'
       : '',
 }
 
